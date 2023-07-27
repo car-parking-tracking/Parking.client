069476ffc2dd75d03ffe1db14de42d24a6b69832
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-07-27 11:21:56 +0300

    refactor: add comments, remove dotenv import

--- src/components/organisms/ya-map/ya-map.component.tsx
+++ src/components/organisms/ya-map/ya-map.component.tsx
@@ -1,9 +1,8 @@
-// import 'dotenv/config'
-
 import { Map, YMaps } from '@pbe/react-yandex-maps'
 import { Wrapper } from './ya-map.styles'
 import { mapConfig } from './ya-map.config'
 
+// TODO - подулючить ключ апи из переменных окружения
 // const { YAMAP_API_KEY = '' } = process.env
 
 export const YaMap: React.FC = () => {
@@ -11,9 +10,13 @@ export const YaMap: React.FC = () => {
     <Wrapper>
       <YMaps
         query={{
-          apikey: '',
+          apikey: '', // TODO использовать здесь YAMAP_API_KEY вместо ''
         }}>
-        <Map {...mapConfig}></Map>
+        <Map {...mapConfig}>
+          {/*
+          // TODO здесь будут метки  
+         */}
+        </Map>
       </YMaps>
     </Wrapper>
   )
