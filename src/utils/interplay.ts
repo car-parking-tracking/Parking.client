c6441ec2a0a3bf3489a1d1cef2be205742422dd4
mrHoft <ru.hoft@gmail.com>
2023-07-30 02:35:41 +0300

    feat: Interplay class methods

--- src/utils/interplay.ts
+++ src/utils/interplay.ts
@@ -3,48 +3,49 @@
  * Custom context provider and state manager
  */
 
-type Sructure = Record<string, () => void>
-const _void = () => {}
+type Indexed<T = unknown> = { [key in string | symbol]: T }
 
-const defaultSructure: Sructure = {
-  'favorites.show': _void,
-  'menu.show': _void,
-  'registration.show': _void,
+const defaultSructure: Indexed = {
+  'favorites.show': () => {},
+  'menu.show': () => {},
+  'registration.show': () => {},
 }
 
 class Interplay {
-  private _instance: Interplay | null = null
-  private state: Record<string, Sructure> = { null: { null: _void } }
+  private static _instance: Interplay
+  private state: Indexed = { null: { null: null } }
 
-  constructor(sructure?: Sructure) {
-    if (this._instance) return this._instance
+  constructor(sructure?: Indexed) {
+    if (Interplay._instance) {
+      if (sructure) this.fill(sructure)
+      return Interplay._instance
+    }
+    Interplay._instance = this
     if (sructure) this.fill(sructure)
-    this._instance = new Interplay(defaultSructure)
+    else this.fill(defaultSructure)
   }
 
-  private fill = (data: Sructure) => {
-    for (const key in data) addValue(this.state, key, data[key])
+  private fill = (data: Indexed) => {
+    for (const key in data) this.add(key, data[key])
   }
 
-  public print = () => {
-    console.log(this.state)
+  public get = (path: string) => {
+    return path.split('.').reduce((obj: Indexed | null, key: string) => (obj && obj[key] ? (obj[key] as Indexed) : null), this.state) as unknown
   }
-}
 
-type Indexed<T = unknown> = { [key in string | symbol]: T }
+  public run = (path: string) => {
+    const val = this.get(path)
+    if (typeof val === 'function') val()
+  }
 
-const isPrimitive = (val: unknown): boolean => val !== Object(val)
-
-function addValue(object: Indexed | unknown, path: string, value: unknown): Indexed | unknown {
-  if (isPrimitive(object)) return object
-  if (typeof path != 'string' || path == '') throw new Error('path must be string')
-  let obj = object as Indexed
-  const arr = path.split('.')
-  const last = arr.pop() || path
-  arr.forEach(key => {
-    if (!obj[key]) obj[key] = {}
-    obj = obj[key] as Indexed
-  })
-  obj[last] = value
-  return object
+  public add = (path: string, value: unknown): void => {
+    let obj = this.state as Indexed
+    const arr = path.split('.')
+    const last = arr.pop() || path
+    arr.forEach(key => {
+      if (!obj[key]) obj[key] = {}
+      obj = obj[key] as Indexed
+    })
+    obj[last] = value
+  }
 }
