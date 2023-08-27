397c6ee7bd84d9bbeb118b084c03e35b79db5f99
Tatiana <tat.mi.iva@gmail.com>
2023-08-27 22:17:26 +0300

    refactor/25: refactor requests (#47)
    * refactor: port parking lots data request to rtk query; reexport useQueries
    
    * Fix reducer and store
    
    ---------
    
    Co-authored-by: shinonhorror <shinonhorror@gmail.com>

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -3,11 +3,12 @@ import { baseApi } from './api/baseApi'
 import { rootReducer } from './reducer'
 import { collectionApi } from './api/collection/collectionApi'
 import { geocodeApi } from './api/geocoder/geocoderApi'
+import { lotsApi } from './api'
 
 export const setupStore = () => {
   return configureStore({
     reducer: rootReducer,
-    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware, collectionApi.middleware, geocodeApi.middleware),
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware, collectionApi.middleware, geocodeApi.middleware, lotsApi.middleware),
   })
 }
 
