bb2e6f2643b8d1b5d7b7fb99e8b24a4564a576c1
shinonhorror <shinonhorror@gmail.com>
2023-08-30 12:06:37 +0300

    Fix rerender

--- src/components/organisms/ya-map/ya-map.component.tsx
+++ src/components/organisms/ya-map/ya-map.component.tsx
@@ -16,11 +16,9 @@ import { useFetchFeatureCollectionQuery, useFetchGeocodeDataQuery, useFetchLotBy
 
 export const YaMap: React.FC = () => {
   const [activePortal, setActivePortal] = useState<boolean>(false)
-  const [manager, setManager] = useState<AnyObject>()
   const [newCoords, setNewCoords] = useState([55.751774, 37.61838])
   const [value, setValue] = useState('')
   const [options, setOptions] = useState<any[]>([])
-  const [activeParkingData, setActiveParkingData] = useState<AnyObject | null>(null)
   const [parkingID, setParkingID] = useState<number | null>(null)
   const [zoom, setZoom] = useState(16)
 
@@ -29,53 +27,29 @@ export const YaMap: React.FC = () => {
   const { data: geocodeData } = useFetchGeocodeDataQuery(value, {
     skip: !value,
   })
-  
+
   const { data: lotData } = useFetchLotByIdQuery(parkingID ?? skipToken)
-
-  useEffect(() => {
-    lotData && setActiveParkingData(lotData)
-  }, [lotData])
-
+  
   const handleOpenBalloon = (e: IEvent) => {
     const parkingID = e.get('objectId')
     if (typeof parkingID !== 'number') return
+
     setParkingID(parkingID)
 
-    manager?.objects.setObjectOptions(parkingID, {
+    e.get('target').getOwner().setObjectOptions(parkingID, {
       iconImageHref: placemarkActive,
     })
-    setTimeout(() => {
-      setActivePortal(true)
-    }, 0)
+
+    setActivePortal(true)
   }
 
   const handleCloseBalloon = (e: IEvent) => {
-    manager?.objects.setObjectOptions(e.get('objectId'), {
+    e.get('target').getOwner().setObjectOptions(parkingID, {
       iconImageHref: placemark,
     })
+
     setActivePortal(false)
-    setActiveParkingData(null)
   }
-
-  useEffect(() => {
-    manager?.objects.balloon.events.add('open', (e: IEvent) => {
-      handleOpenBalloon(e)
-    })
-
-    manager?.objects.balloon.events.add('close', (e: IEvent) => {
-      handleCloseBalloon(e)
-    })
-
-    return () => {
-      manager?.objects.balloon.events.remove('open', (e: IEvent) => {
-        handleOpenBalloon(e)
-      })
-
-      manager?.objects.balloon.events.remove('close', (e: IEvent) => {
-        handleCloseBalloon(e)
-      })
-    }
-  }, [manager])
 
   useEffect(() => {
     if (geocodeData) {
@@ -112,22 +86,36 @@ export const YaMap: React.FC = () => {
             center: newCoords,
             zoom: zoom,
           }}>
-          {data && <ObjectManager {...managerConfig} defaultFeatures={JSON.stringify(data)} instanceRef={(ref: AnyObject) => setManager(ref)} />}
+          {data && (
+            <ObjectManager
+              {...managerConfig}
+              defaultFeatures={JSON.stringify(data)}
+              instanceRef={(ref: AnyObject) => {
+                if (ref) {
+                  ref.objects.balloon.events.add('open', (e: IEvent) => {
+                    handleOpenBalloon(e)
+                  })
 
-          <ZoomControl options={{ position: { right: 32, top: 90 }, size: 'large' }} />
-
-          {activePortal && (
-            <Portal getHTMLElementId={'parking'}>
-              <ParkingCard
-                id={activeParkingData?.id || 0}
-                address={activeParkingData?.address || 'Нет данных'}
-                carCapacity={activeParkingData?.car_capacity || 'Нет данных'}
-                tariffs={[{ hourPrice: 100 }]}
-              />
-            </Portal>
+                  ref.objects.balloon.events.add('close', (e: IEvent) => {
+                    handleCloseBalloon(e)
+                  })
+                }
+              }}
+            />
           )}
+          <ZoomControl options={{ position: { right: 32, top: 90 }, size: 'large' }} />
         </Map>
       </YMaps>
+      {activePortal && (
+        <Portal getHTMLElementId={'parking'}>
+          <ParkingCard
+            id={lotData?.id || 0}
+            address={lotData?.address || 'Нет данных'}
+            carCapacity={lotData?.car_capacity || 'Нет данных'}
+            tariffs={[{ hourPrice: 100 }]}
+          />
+        </Portal>
+      )}
     </Wrapper>
   )
 }
