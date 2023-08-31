66f304aaca0491e7ef52fb211270589617d4a292
Alexander Filimonov <fil.dsgn@yandex.ru>
2023-08-31 16:18:15 +0300

    dev: update input form

--- src/components/molecules/inputForm/inputForm.styles.tsx
+++ src/components/molecules/inputForm/inputForm.styles.tsx
@@ -5,33 +5,19 @@ export const Container = styled.div`
   position: relative;
   display: flex;
   flex-direction: column;
+  gap: 0.125rem;
 `
 export const Label = styled.label`
-  position: absolute;
+  font-size: 0.875rem;
   pointer-events: none;
-  left: 1rem;
-  top: 1.313rem;
   transition: 0.3s ease all;
   color: rgba(27, 31, 59, 0.65);
-
-  ${Input}:focus ~ & {
-    font-size: 0.813rem;
-    line-height: 1.25rem;
-    left: 1rem;
-    top: 0.375rem;
-  }
-
-  ${Input}:not(:placeholder-shown) ~ & {
-    font-size: 0.813rem;
-    line-height: 1.25rem;
-    left: 1rem;
-    top: 0.375rem;
-  }
 `
 export const StyledInput = styled(Input)`
   ::placeholder {
     opacity: 0;
   }
+  margin: 0;
 `
 export const Span = styled.span`
   margin-top: 0.438rem;
