b995c037cc9bf588be8c5abe83cc91a26b4b3c07
Maria <57846361+shinonhorror@users.noreply.github.com>
2023-09-01 23:09:32 +0400

    Feat/26 (#50)
    * Add global styles
    
    * search component story
    
    * ParkingCard
    
    * text styles
    
    * Fix buttons storybook
    
    ---------
    
    Co-authored-by: alheym <alheym.mw@gmail.com>

--- src/components/atoms/button/button.styles.tsx
+++ src/components/atoms/button/button.styles.tsx
@@ -1,5 +1,7 @@
 import styled, { css } from 'styled-components'
 import { ButtonProps } from './button.types'
+import LikeInactive from '@assets/icons/like-inactive.svg'
+import LikeActive from '@assets/icons/like-active.svg'
 
 const shared = css<ButtonProps>`
   color: var(--txt-white);
@@ -8,29 +10,93 @@ const shared = css<ButtonProps>`
   transition: 0.3s ease-in-out;
   font-weight: 600;
   border-radius: 0.75rem;
-  padding: 0rem 1.5rem;
-  height: 3.5rem;
+  padding: 12px 16px;
 `
 const secondary = css<ButtonProps>`
   ${shared}
-  border: 0.0625rem solid transparent;
+  border: 0.125rem solid transparent;
   background-color: var(--grey);
   box-shadow: 0rem 0.25rem 0.313rem rgba(0, 0, 0, 0.1);
 
   &:hover {
     background-color: var(--button-bg-default);
   }
+
+  &:disabled {
+    background-color: var(--search-item-hover);
+    color: var(--grey);
+  }
 `
+const primary = css<ButtonProps>`
+  ${shared}
+  background-color: var(--button-bg-default);
+  border: none;
+  box-shadow: 0rem 0.25rem 0.313rem rgba(0, 0, 0, 0.1);
+
+  &:hover {
+    background-color: var(--button-bg-hover);
+  }
+
+  &:disabled {
+    background-color: var(--search-item-hover);
+    color: var(--grey);
+  }
+`
+
+const icon = css<ButtonProps>`
+  ${shared}
+  background-color: var(--button-bg-default);
+  border: none;
+  box-shadow: 0rem 0.25rem 0.313rem rgba(0, 0, 0, 0.1);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.75rem;
+  font-size: 0.875rem;
+  font-weight: 600;
+  line-height: 1.5rem;
+
+  #masked {
+    width: 20px;
+    height: 20px;
+    mask-image: url(${LikeInactive});
+    background: #fff;
+    transition: 0.3s ease-in-out;
+  }
+
+  &:hover {
+    background-color: var(--button-bg-hover);
+    #masked {
+      mask-image: url(${LikeActive});
+    }
+  }
+
+  &:disabled {
+    background-color: var(--search-item-hover);
+    color: var(--grey);
+    #masked {
+      background: var(--grey);
+      mask-image: url(${LikeInactive});
+    }
+  }
+`
+
 const outlined = css<ButtonProps>`
   ${shared}
   color: var(--bg-active);
   background-color: transparent;
-  border: 0.0625rem solid var(--button-bg-default);
-  box-shadow: 0rem 0.25rem 0.3125rem rgba(0, 0, 0, 0.1);
+  border: 0.125rem solid var(--button-bg-default);
+  box-shadow: 0rem 0.25rem 0.313rem rgba(0, 0, 0, 0.1);
 
   &:hover {
     color: var(--txt-white);
     background-color: var(--button-bg-default);
+  }
+
+  &:disabled {
+    border: 0.125rem solid var(--search-item-hover);
+    background-color: transparent;
+    color: var(--bg-active);
   }
 `
 
@@ -39,8 +105,12 @@ export const Button = styled.button<ButtonProps>`
     switch (variant) {
       case 'secondary':
         return secondary
+      case 'primary':
+        return primary
       case 'outlined':
         return outlined
+      case 'icon':
+        return icon
     }
   }}
 `
