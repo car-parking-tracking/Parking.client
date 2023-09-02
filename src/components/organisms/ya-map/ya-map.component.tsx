72ff46f6192e8322a5fb0bf2fea2118e80540013
alheym <alheym.mw@gmail.com>
2023-09-02 13:36:44 +0300

    fix

--- src/components/organisms/ya-map/ya-map.component.tsx
+++ src/components/organisms/ya-map/ya-map.component.tsx
@@ -16,11 +16,9 @@ import { useFetchFeatureCollectionQuery, useFetchGeocodeDataQuery, useFetchLotBy
 
 export const YaMap: React.FC = () => {
   const [activePortal, setActivePortal] = useState<boolean>(false)
-  const [manager, setManager] = useState<AnyObject>()
   const [newCoords, setNewCoords] = useState([55.751774, 37.61838])
   const [value, setValue] = useState('')
   const [options, setOptions] = useState<any[]>([])
-  const [activeParkingData, setActiveParkingData] = useState<AnyObject | null>(null)
   const [parkingID, setParkingID] = useState<number | null>(null)
   const [zoom, setZoom] = useState(16)
 
@@ -36,58 +34,36 @@ export const YaMap: React.FC = () => {
     skip: !value,
   })
 
-  useEffect(() => {
-    lotData && setActiveParkingData(lotData)
-  }, [lotData])
-
   const handleOpenBalloon = (e: IEvent) => {
     const parkingID = e.get('objectId')
     if (typeof parkingID !== 'number') return
+
     setParkingID(parkingID)
 
-    manager?.objects.setObjectOptions(parkingID, {
+    e.get('target').getOwner().setObjectOptions(parkingID, {
       iconImageHref: placemarkActive,
     })
-    setTimeout(() => {
-      setActivePortal(true)
-    }, 0)
+
+    setActivePortal(true)
   }
 
   const handleCloseBalloon = (e: IEvent) => {
-    manager?.objects.setObjectOptions(e.get('objectId'), {
+    e.get('target').getOwner().setObjectOptions(parkingID, {
       iconImageHref: placemark,
     })
+
     setActivePortal(false)
-    setActiveParkingData(null)
   }
-
-  useEffect(() => {
-    manager?.objects.balloon.events.add('open', (e: IEvent) => {
-      handleOpenBalloon(e)
-    })
-
-    manager?.objects.balloon.events.add('close', (e: IEvent) => {
-      handleCloseBalloon(e)
-    })
-
-    return () => {
-      manager?.objects.balloon.events.remove('open', (e: IEvent) => {
-        handleOpenBalloon(e)
-      })
-
-      manager?.objects.balloon.events.remove('close', (e: IEvent) => {
-        handleCloseBalloon(e)
-      })
-    }
-  }, [manager])
 
   useEffect(() => {
     if (lotsCollectionData) {
       const lotsCollection = lotsCollectionData.results.map((lot: any) => {
+        const nameParking = `Парковка № ${lot.id}`
         return {
-          name: lot.id,
+          name: nameParking,
           description: lot.address,
           coords: [lot.latitude, lot.longitude],
+          id: lot.id
         }
       })
       setOptions(() => [...lotsCollection])
@@ -98,21 +74,21 @@ export const YaMap: React.FC = () => {
     if (geocodeData) {
       const collection = geocodeData.response.GeoObjectCollection.featureMember.map((item: any) => item.GeoObject)
       setOptions(() => collection)
-      console.log(collection)
     }
   }, [geocodeData])
 
   const handleInputChange = (newValue: string) => {
+    newValue = newValue.replace('Парковка №', '')
     setValue(newValue)
   }
 
   const handleOptionClick = (newValue: string) => {
     let obg
 
-    if (isNaN(Number(newValue))) {
+    if (!isNaN(Number(newValue))) {
       obg = options.find(item => newValue.includes(item.name))
     } else {
-      obg = options.find(item => item.name === Number(newValue))
+      obg = options.find(item => item.id === Number(newValue.replace('Парковка № ', '')))
     }
 
     let coords
@@ -126,8 +102,8 @@ export const YaMap: React.FC = () => {
         setZoom(21)
       } else {
         setNewCoords(obg.coords)
-        setParkingID(obg.name)
-        //manager?.objects.balloon.open(Number(obg.name))
+        setParkingID(obg.id)
+        //manager?.objects.balloon.open(Number(obg.name)) //TODO: доделать открытие балуна
         setZoom(21)
       }
     }
@@ -148,22 +124,36 @@ export const YaMap: React.FC = () => {
             center: newCoords,
             zoom: zoom,
           }}>
-          {data && <ObjectManager {...managerConfig} defaultFeatures={JSON.stringify(data)} instanceRef={(ref: AnyObject) => setManager(ref)} />}
+          {data && (
+            <ObjectManager
+              {...managerConfig}
+              defaultFeatures={JSON.stringify(data)}
+              instanceRef={(ref: AnyObject) => {
+                if (ref) {
+                  ref.objects.balloon.events.add('open', (e: IEvent) => {
+                    handleOpenBalloon(e)
+                  })
 
-          <ZoomControl options={{ position: { right: 32, top: 90 }, size: 'large' }} />
-
-          {activePortal && (
-            <Portal getHTMLElementId={'parking'}>
-              <ParkingCard
-                id={activeParkingData?.id || 0}
-                address={activeParkingData?.address || 'Нет данных'}
-                carCapacity={activeParkingData?.car_capacity || 'Нет данных'}
-                tariffs={[{ hourPrice: 100 }]}
-              />
-            </Portal>
+                  ref.objects.balloon.events.add('close', (e: IEvent) => {
+                    handleCloseBalloon(e)
+                  })
+                }
+              }}
+            />
           )}
+          <ZoomControl options={{ position: { right: 32, top: 90 }, size: 'large' }} />
         </Map>
       </YMaps>
+      {activePortal && (
+        <Portal getHTMLElementId={'parking'}>
+          <ParkingCard
+            id={lotData?.id || 0}
+            address={lotData?.address || 'Нет данных'}
+            carCapacity={lotData?.car_capacity || 'Нет данных'}
+            tariffs={[{ hourPrice: 100 }]}
+          />
+        </Portal>
+      )}
     </Wrapper>
   )
 }
