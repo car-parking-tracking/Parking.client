06e943708d23096523abca81f5e5240e72215905
shinonhorror <shinonhorror@gmail.com>
2023-09-25 11:03:16 +0300

    add base path

--- src/constants/environment.ts
+++ src/constants/environment.ts
@@ -1,3 +1,4 @@
 const YAMAP_API_KEY = process.env.YAMAP_API_KEY
-
-export { YAMAP_API_KEY }
+const BASE_URL_PROD = process.env.BASE_URL_PROD
+const BASE_URL_DEV = process.env.BASE_URL_DEV
+export { YAMAP_API_KEY, BASE_URL_PROD, BASE_URL_DEV }
