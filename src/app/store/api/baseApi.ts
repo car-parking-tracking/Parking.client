1e1bf1e8ec7adaeb51169872c59705e54b9b559a
shinonhorror <shinonhorror@gmail.com>
2023-09-27 13:52:46 +0300

    change base api path

--- src/app/store/api/baseApi.ts
+++ src/app/store/api/baseApi.ts
@@ -1,11 +1,9 @@
 import { BASE_URL_DEV, BASE_URL_PROD } from '@constants/environment'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-// export const BASE_API_PATH = process.env.NODE_ENV === 'development' ? BASE_URL_DEV : BASE_URL_PROD
+export const BASE_API_PATH = `${window.location.protocol}//${window.location.hostname}/api/v1/`
 
-// export const BASE_API_PATH = `${window.location.protocol}//${window.location.hostname}/api/v1/`
-
-export const BASE_API_PATH = 'https://parkonaft.ru/api/v1/'
+// export const BASE_API_PATH = 'https://parkonaft.ru/api/v1/'
 
 export const baseQuery = fetchBaseQuery({
   baseUrl: `${BASE_API_PATH}`,
